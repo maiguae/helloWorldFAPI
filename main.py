46018704dd53ce437fb70be2f7546a74cbf7de5e
maiguae <maiguae03@gmail.com>
2025-05-15 15:57:30 -0500

    Deploy FastAPI app

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# main.py
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "¡Hola Mundo desde FastAPI!"}
+    return {"Welcome": "Hello World"}
+
